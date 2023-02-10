9a71ceb34d76f4eb65051f0a897065319a8f3ed8
Arth Shukla <44527268+arth-shukla@users.noreply.github.com>
2023-02-10 13:05:10 -0800

    Progress: header and footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,17 @@ import React from 'react'
 import './App.scss'
 
 function App() {
-	return <></>
+	return (
+		<>
+			<header>
+				<h1>Lorem ipsum</h1>
+			</header>
+			<div className='main-content'></div>
+			<footer>
+				<p>Contact</p>
+			</footer>
+		</>
+	)
 }
 
 export default App
