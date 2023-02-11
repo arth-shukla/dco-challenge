0b4e0f11c995203240ec51b0fc5a8acf893d65ef
Arth Shukla <44527268+arth-shukla@users.noreply.github.com>
2023-02-10 21:25:56 -0800

    Progress: use window.innerHeight for mobile compatibility

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.scss'
 import { ContactLinks, Content, Infobar, Sidebar } from './components'
 
 function App() {
+	// ----------------------------------------------------
+	// NOTE: A similar thing can be acheived in CSS via the following:
+	//     min-height: calc(100vh - 2 * v.$site-margin);
+	// HOWEVER, I opt not to use vh due to poor compatability
+	//     with mobile/tablet versions of Safari
+	// ----------------------------------------------------
+	const [winHeight, setWinHeight] = useState<number>(window.innerHeight)
+	useEffect(() => {
+		window.addEventListener('resize', setWinHeight.bind(undefined, window.innerHeight), { passive: true })
+	}, [])
+
 	return (
-		<div className='App'>
+		<div
+			className='App'
+			style={{ minHeight: winHeight - 20 }}
+		>
 			<header>
 				<h1>Lorem ipsum</h1>
 			</header>
